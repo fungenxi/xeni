16d719e7340496bb2779dc70705af6ac8fbd1d29
fungenxi <fungenxii@gmail.com>
2026-08-17 17:42:45 +0800

    Keep analytics menus above the chart

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'finance-tracker-7f73e5a72c579e1d';
-const PRECACHE = ["assets/index-7f73e5a7.js","assets/index-2c579e1d.css","icon.svg","index.html","manifest.webmanifest"];
+const CACHE_NAME = 'finance-tracker-7f73e5a7809063fa';
+const PRECACHE = ["assets/index-7f73e5a7.js","assets/index-809063fa.css","icon.svg","index.html","manifest.webmanifest"];
 const urlFor = (file) => new URL(file, self.registration.scope).href;
 
 self.addEventListener('install', (event) => {
